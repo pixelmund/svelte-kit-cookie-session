0a1c1a35e8000564c738287bead3057d2bf21ff2
pixelmund <pixelmund@gmail.com>
2021-03-31 20:23:20 +0200

    Fix stuff

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { default as initializeSession } from "./initialize";
 export { daysToMaxage } from "./utils/cookie";
+export { initializeSession as setupSessionClient } from "./runtime/browser";
+export * from "./runtime/server";
