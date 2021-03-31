3e7c17c6230ec391d0489b63d6480702015f395c
pixelmund <pixelmund@gmail.com>
2021-03-31 20:28:42 +0200

    Revert everything

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 export { default as initializeSession } from "./initialize";
 export { daysToMaxage } from "./utils/cookie";
-export { initializeSession as setupSessionClient, session } from "./runtime/browser";
-export * from "./runtime/server";
