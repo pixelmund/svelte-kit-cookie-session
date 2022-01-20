6ea291b61496fae0d21bf2718a8630c50362894e
pixelmund <pixelmund@gmail.com>
2022-01-20 21:12:39 +0100

    Export connect middleware

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as cookieSession } from './core.js';
 export { handleSession } from './handle.js';
+export { sessionMiddleware } from "./connect.js"
 
 export type { SessionOptions, Session } from './types';
